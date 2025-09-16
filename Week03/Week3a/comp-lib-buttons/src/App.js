4ecef15b37702c5b7a7ec2a21c5bfaad12f6dd17
Kathryn Adee <katie.adee@terrycontracting.com>
2025-09-16 10:28:01 -0400

    Add notes and tailwind merge example

--- Week03/Week3a/comp-lib-buttons/src/App.js
+++ Week03/Week3a/comp-lib-buttons/src/App.js
@@ -1,23 +1,45 @@
 import Button from './components/Button'
 
-const App = () =>{
+const App = () => {
   return (
     <>
-    <div>
-      <Button primary outline>Buy Now</Button>
-    </div>
-    <div>
-      <Button secondary rounded>Secondary Button</Button>
-          </div>
-    <div>
-      <Button danger>Delete</Button>
-          </div>
-    <div>
-      <Button warning outline rounded>Are you sure?</Button>
-          </div>
-    <div>
-      <Button success outline>Success</Button>
-    </div>
+      <div>
+        <Button
+          primary
+          danger
+          outline
+        >
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Button
+          secondary
+          rounded
+        >
+          Secondary Button
+        </Button>
+      </div>
+      <div>
+        <Button danger>Delete</Button>
+      </div>
+      <div>
+        <Button
+          warning
+          outline
+          rounded
+        >
+          Are you sure?
+        </Button>
+      </div>
+      <div>
+        <Button
+          success
+          outline
+        >
+          Success
+        </Button>
+      </div>
     </>
   )
 }
