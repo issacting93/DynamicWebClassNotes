52e9adebd4b9a39974d9864ec42615d98b210b58
Kathryn Adee <katie.adee@terrycontracting.com>
2025-09-16 09:52:36 -0400

    Add button component to comp-lib

---
+++ Week03/Week3a/comp-lib-buttons/src/App.js
@@ -0,0 +1,25 @@
+import Button from './components/Button'
+
+const App = () =>{
+  return (
+    <>
+    <div>
+      <Button primary outline>Buy Now</Button>
+    </div>
+    <div>
+      <Button secondary rounded>Secondary Button</Button>
+          </div>
+    <div>
+      <Button danger>Delete</Button>
+          </div>
+    <div>
+      <Button warning outline rounded>Are you sure?</Button>
+          </div>
+    <div>
+      <Button success outline>Success</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ Week03/Week3a/comp-lib-buttons/src/components/Button.js
@@ -0,0 +1,45 @@
+import cx from 'classnames'
+// import PropTypes from 'prop-types'; // ES6
+
+const Button = (props) => {
+    const {children, primary, secondary, success, warning, danger, rounded, outline} = props
+    
+    const baseClass = "flex items-center px-8 py-3 border"
+    return <button className={cx(baseClass, {
+        // color variants, pick one
+        'bg-blue-500 border-blue-500 text-white': primary,
+        'bg-gray-900 border-gray-900 text-white': secondary,
+        'bg-green-500 border-green-500 text-white': success,
+        'bg-orange-400 border-orange-500 text-white': warning,
+        'bg-red-600 border-red-600 text-white': danger,
+        // outline and rounded additional style props
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-orange-400': outline && warning,
+        'text-red-600': outline && danger,
+    })}>{children}</button>
+}
+    // validating props by type, this is the most commonb reason for using this library
+    // typscript is also used for this more recently and a lot more
+    // primary: PropTypes.bool,
+Button.propTypes = {
+    customPropValidation:( {primary, secondary, success, warning, danger}) => {
+        const count = 
+        Number(!!primary)
+        + Number(!!secondary)
+        + Number(!!success)
+        + Number(!!warning)
+        + Number(!!danger)
+
+        console.log(count)
+
+        if (count > 1){
+            return new Error('You silly goose! Only one of primary, secondary, success, warning, danger can be TRUE!')
+        }
+    }
+} 
+
+export default Button
